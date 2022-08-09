8e670b6579d53bc39d484c05114cd00507bbf467
ekremkenter <ekremkenter@gmail.com>
2022-08-09 13:36:59 +0300

    Add chatwoot

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ function Document() {
   return (
     <Html>
       <Head>
-        <Script id='gtm' strategy="afterInteractive" dangerouslySetInnerHTML={{
+        <Script id='gtm' strategy='afterInteractive' dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -20,6 +20,24 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       }}></noscript>
       <Main/>
       <NextScript/>
+      <Script id='chatwoot' strategy='afterInteractive'>
+        {`
+        (function(d,t) {
+          var BASE_URL="https://app.chatwoot.com";
+          var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+          g.src=BASE_URL+"/packs/js/sdk.js";
+          g.defer = true;
+          g.async = true;
+          s.parentNode.insertBefore(g,s);
+          g.onload=function(){
+            window.chatwootSDK.run({
+              websiteToken: 'kzxmCYaGSi8NWNK6CEkeGMTR',
+              baseUrl: BASE_URL
+            })
+          }
+        })(document,"script");
+        `}
+      </Script>
       </body>
     </Html>
   );
